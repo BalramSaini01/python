bf5eef86a78c58725b15c828c16e9bc9a87ec20a
BalramSaini01 <161507508+BalramSaini01@users.noreply.github.com>
2024-05-11 19:16:04 +0530

    Delete 1.if_statement.py

--- 1.if_statement.py
+++
@@ -1,6 +0,0 @@
-"""The if statement is the most simple decision-making statement.
-It is used to decide whether a certain statement or block of statements will be executed or not."""
-num=int(input("enter your age :"))
-if num>=18:
-    print("you are eligable for vote")
-    
